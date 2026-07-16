8a400339437a1693510570ef7f8cf6d5346036a8
zaskalet-site <zaskalet@gmail.com>
2026-07-16 18:41:02 +0300

    Add files via upload

---
+++ c/a7K9mQ2xR8vL4pZ6/assets/config.js
@@ -0,0 +1,26 @@
+window.CARD_CONFIG = {
+  fullName: "Mikhail Zaskalet",
+  title: "Technology Entrepreneur",
+  organization: "",
+  phone: "",
+  website: "",
+
+  linkedin: "https://www.linkedin.com/in/mzaskalet/",
+  facebook: "https://www.facebook.com/mzaskalet",
+  instagram: "https://www.instagram.com/zaskalet/",
+  instagramHandle: "@zaskalet",
+
+  whatsapp: "https://wa.me/message/U27I67VXRLP5I1",
+  telegram: "https://t.me/MIKHAIL_NNN",
+  telegramHandle: "@MIKHAIL_NNN",
+
+  gamingEmail: "bogdan2kmail@gmail.com",
+  amateurRadioEmail: "5b4alj@gmail.com",
+  amateurRadioCallsign: "5B4ALJ",
+  discordUsername: "Bogdan2K",
+
+  // Address used by the "Share Your Details" form.
+  // Business/general replies currently go to the gaming email until a separate
+  // personal/business email is added.
+  shareDetailsRecipient: "bogdan2kmail@gmail.com"
+};
